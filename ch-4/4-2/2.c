e4ef29566be621fb74a8881ed6aa74d82aa2da1f
Darshan Hadiyal <146731392+imhadiyal@users.noreply.github.com>
2023-10-18 20:12:55 +0530

    2.c

--- ch-4/4-2/2.c
+++ ch-4/4-2/2.c
@@ -9,9 +9,9 @@ int main()
     printf("Enter the value b: ");
     scanf("%d", &b);
 
-    a = a ^ b;
-    b = a ^ b;
-    a = a ^ b;
+    a = a + b;
+    b = a - b;
+    a = a - b;
 
     printf("After swapping:\n");
     printf("a = %d\n", a);
