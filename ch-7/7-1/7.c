58b4548297ff32f747a0388ea241ae37780773b4
Darshan Hadiyal <146731392+imhadiyal@users.noreply.github.com>
2023-10-18 12:32:30 +0530

    26-9-2023

---
+++ ch-7/7-1/7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 5;
+    char Char = 'A';
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%c ", Char);
+            Char++;
+        }
+
+        printf("\n");
+
+        Char = 'A' + i;
+    }
+
+    return 0;
+}
