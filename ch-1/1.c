723653a054da9e27892eee2c98406e0418714e4d
Darshan Hadiyal <146731392+imhadiyal@users.noreply.github.com>
2023-11-06 09:53:15 +0530

    29-8-2023

---
+++ ch-1/1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+	clrscr();
+	printf("NAME   : darshan hadiyal");
+	printf("AGE    : 19");
+	printf("college : BMU");
+	getch();
+
+
+}
---
+++ ch-1/2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    clrscr();
+    printf("name\t: darshan hadiyal\n");
+    printf("age\t:18\n");
+    printf("school\t: shreemati hematlal lokhand wala\n");
+    getch();
+
+
+
+
+}
